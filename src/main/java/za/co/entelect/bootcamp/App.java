ad783cf1c75b44828b2cd336534738da35332906
quinton.weenink <quinton.weenink@entelect.co.za>
2017-01-11 14:32:56 +0200

    Changed the code

--- src/main/java/za/co/entelect/bootcamp/App.java
+++ src/main/java/za/co/entelect/bootcamp/App.java
@@ -5,7 +5,7 @@ public class App
     public static final double CURRENT_RATE = 22.07;
 
     public static double convertRandToDollar(double value){
-        return value * CURRENT_RATE / 100;
+        return value / CURRENT_RATE;
     }
 
 }
