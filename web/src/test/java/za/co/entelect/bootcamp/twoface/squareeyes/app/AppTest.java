124c12dfd9f3bf715969802514c760d0370f282f
quinton.weenink <quinton.weenink@entelect.co.za>
2017-01-16 13:11:59 +0200

    Added other modules

---
+++ web/src/test/java/za/co/entelect/bootcamp/twoface/squareeyes/app/AppTest.java
@@ -0,0 +1,26 @@
+package za.co.entelect.bootcamp.twoface.squareeyes.app;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    @Before
+    public void print(){
+        System.out.println("=== Two face ===");
+        System.out.println("Members:");
+	    System.out.println("- Mpho Mahase!");
+        System.out.println("- Quinton Weenink!");
+	    System.out.println("- Sean Vienings!");
+    }
+
+    @Test
+    public void givenRandValueWhenConvertedToDollarExpectCorrectConversion() {
+        double convertedToDollar = App.convertRandToDollar(7.22);
+        assertEquals(convertedToDollar,0.3271409152695967, 0);
+    }
+
+}
