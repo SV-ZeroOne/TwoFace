659a7ab5302bc52fe635fd2dee1476f74c96eb47
Mpho Mahase <mpho.mahase@entelect.co.za>
2017-01-16 17:18:33 +0200

    Added Classes to assist with the adapter

--- services/src/main/java/za/co/entelect/bootcamp/twoface/squareeyes/services/IssueOrder.java
+++ services/src/main/java/za/co/entelect/bootcamp/twoface/squareeyes/services/IssueOrder.java
@@ -1,38 +1,43 @@
 package za.co.entelect.bootcamp.twoface.squareeyes.services;
 
-public class IssueOrderDTO {
-
-    private int issueID;
-    private String seriesNumber;
-    private int quantity;
-
-    public IssueOrderDTO(int issueID, String seriesNumber, int quantity) {
-        this.issueID = issueID;
-        this.seriesNumber = seriesNumber;
-        this.quantity = quantity;
-    }
-
+/**
+ * Created by mpho.mahase on 2017/01/16.
+ */
+public class IssueOrder implements SupplierService{
+/*
+    IssueOrderDTO
     public int getIssueID() {
         return issueID;
     }
-
     public void setIssueID(int issueID) {
         this.issueID = issueID;
     }
-
     public String getSeriesNumber() {
         return seriesNumber;
     }
-
     public void setSeriesNumber(String seriesNumber) {
         this.seriesNumber = seriesNumber;
     }
-
     public int getQuantity() {
         return quantity;
     }
-
     public void setQuantity(int quantity) {
         this.quantity = quantity;
+    }
+*/
+
+    /*
+    Order
+    private int orderID;
+    private Date orderDate;
+    private long total;
+    private String shipmentRef;
+    private Date shipmentDate;
+    private String deliveryStatus;
+    private Map<Issue, Integer> issueList;
+    private Supplier supplier;
+    */
+    public String placeOrder(IssueOrderDTO issueOrder, String supplierRefNumber, int quantity) {
+        return null;
     }
 }
