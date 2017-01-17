701553af5909179c1e9afd5ae138a4c1e5dd4b8c
Mpho Mahase <mpho.mahase@entelect.co.za>
2017-01-17 10:52:49 +0200

    Added dummy data to the OrderRepositoryIMP

--- persistence/src/main/java/za/co/entelect/bootcamp/twoface/squareeyes/persistence/OrderRepository.java
+++ persistence/src/main/java/za/co/entelect/bootcamp/twoface/squareeyes/persistence/OrderRepository.java
@@ -6,5 +6,6 @@ import za.co.entelect.bootcamp.twoface.squareeyes.domain.supplier.Supplier;
  * Created by sean.vienings on 2017/01/16.
  */
 public interface OrderRepository<Order, Integer>  extends Repository{
+
 }
 
--- persistence/src/main/java/za/co/entelect/bootcamp/twoface/squareeyes/persistence/OrderRepositoryIMP.java
+++ persistence/src/main/java/za/co/entelect/bootcamp/twoface/squareeyes/persistence/OrderRepositoryIMP.java
@@ -1,7 +1,19 @@
 package za.co.entelect.bootcamp.twoface.squareeyes.persistence;
 
+import za.co.entelect.bootcamp.twoface.squareeyes.domain.issue.Issue;
+import za.co.entelect.bootcamp.twoface.squareeyes.domain.order.Order;
+import za.co.entelect.bootcamp.twoface.squareeyes.domain.supplier.Supplier;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
 /**
  * Created by sean.vienings on 2017/01/16.
  */
 public class OrderRepositoryIMP extends RepositoryBase implements OrderRepository{
+    //public Order (int orderID, Date orderDate, byte qtyOrdered, BigDecimal total, String shipmentRef, Date shipmentDate, String deliveryStatus, Issue issue, Supplier supplier){
+    public OrderRepositoryIMP(Issue issue, Supplier supplier) {
+        tList.put(1, new Order(1, new Date(), (byte)10, new BigDecimal(100), "DMX", new Date(), "In transit", issue, supplier));
+
+    }
 }
