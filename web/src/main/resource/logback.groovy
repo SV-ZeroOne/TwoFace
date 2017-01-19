90442ae52b6d3d71a89bd247cb1c12f51f828f7d
mpho.mahase <mpho.mahase@entelect.co.za>
2017-01-19 16:12:46 +0200

    Added the resource file

---
+++ web/src/main/resource/logback.groovy
@@ -0,0 +1,20 @@
+import ch.qos.logback.core.ConsoleAppender
+import ch.qos.logback.core.FileAppender
+/**
+ * Created by kevin.heritage on 1/19/2017.
+ */
+appender("STDOUT", ConsoleAppender) {
+    append = true
+    encoder(PatternLayoutEncoder) {
+        pattern = "%level %logger - %msg%n"
+    }
+}
+def USER_HOME = System.getProperty("user.home")
+appender("FILE", FileAppender){
+    file = "${USER_HOME}/logger.log"
+    append = true
+    encoder(PatternLayoutEncoder){
+        pattern = "%level %logger - %msg%n"
+    }
+}
+root(INFO, ["FILE"])
