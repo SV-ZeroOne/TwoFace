bb8188e6ff96f23e4966137ef7a2b7f76ecd1265
quinton.weenink <quinton.weenink@entelect.co.za>
2017-01-23 17:00:34 +0200

    Basic product page with stock

--- web/public/product.js
+++ web/public/product.js
@@ -1,16 +1,27 @@
 document.getElementById('remove-me').className = "hide"
 
-var issue = getIssue()
-populateIssue(issue)
+var parameters = location.search.split('&')
+var issueId = parameters[0].split('issue=')[1]
+var stockId = parameters[1].split('stock=')[1]
+
+var issue = getIssue(issueId)
+console.log(issue)
+var stock = getStock(issue, stockId)
+console.log(stock)
+populateIssue(issue, stock)
 
 
-function populateIssue(issue){
-	document.getElementById('title').innerHTML = issue.Publisher + " - " + issue.Title
-	document.getElementById('seriesNumber').innerHTML += issue.SeriesNumber
+function populateIssue(issue, stock){
+	document.getElementById('title').innerHTML = issue.Publisher + " - " + issue.Title + " - #" + issue.SeriesNumber
+	document.getElementById('price').innerHTML += stock.Price
 	document.getElementById('description').innerHTML = issue.Description
-	var date = new Date(issue.PublicationDate);
+	document.getElementById('currentCondition').innerHTML += "Condition: " + stock.Condition
+	for (var i = 0; i < issue.Stock.length; i++) {
+		document.getElementById('condition').innerHTML += "<li><a href='product.html?issue=" + issue.Id + "&stock=" + issue.Stock[i].Id + "'>" + issue.Stock[i].Condition + "</a></li>"
+	}
+	var date = new Date(issue.PublicationDate)
 	document.getElementById('publicationDate').innerHTML = date.getFullYear() + "/" + date.getMonth() + "/" + date.getDay()
-	document.getElementById('addToCartButton').innerHTML += (issue.Stock[0].AvailableQuantity > 0) ? (issue.Stock[0].AvailableQuantity + " Left") : "Out of Stock"
+	document.getElementById('addToCartButton').innerHTML += (stock.AvailableQuantity > 0) ? (stock.AvailableQuantity + " Left") : "Out of Stock"
 }
 
 function addToCart()
@@ -18,18 +29,26 @@ function addToCart()
 	console.log("Adding item to cart")
 }
 
-function getIssue(){
-	var issueId = location.search.split('issue=')[1]
-
+function getIssue(issueId){
 	for (var i = 0; i < issues.length; i++) {
 		if(issues[i].Id == issueId)
 		{
 			return issues[i]
 		}
 	}
-
 	window.location.href = "index.html"
 }
+
+function getStock(issue, stockId){
+	if(!stockId) return
+	for (var i = 0; i < issue.Stock.length; i++) {
+		if(issue.Stock[i].Id == stockId)
+		{
+			return issue.Stock[i]
+		}
+	}
+	return window.location.href = "product.html?issue=" + issue.Id + "&stock=" + issue.Stock[0].Id
+ }
 
 function hideThis(){
 	if(document.getElementById('sidebar').className == "")
