95f2bb61e433aecb32c0d56850154fa751f287ac
Mpho Mahase <mpho.mahase@entelect.co.za>
2017-01-24 20:08:21 +0200

    logback.groovy file

--- web/src/main/resources/logback.groovy
+++ web/src/main/resources/logback.groovy
@@ -1,31 +1,13 @@
-//import ch.qos.logback.core.ConsoleAppender
-//import ch.qos.logback.core.FileAppender
-///**
-// * Created by kevin.heritage on 1/19/2017.
-// */
-//appender("STDOUT", ConsoleAppender) {
-//    append = true
-//    encoder(PatternLayoutEncoder) {
-//        pattern = "%level %logger - %msg%n"
-//    }
-//}
-//def USER_HOME = System.getProperty("user.home")
-//appender("FILE", FileAppender){
-//    file = "${USER_HOME}/logger.log"
-//    append = true
-//    encoder(PatternLayoutEncoder){
-//        pattern = "%level %logger - %msg%n"
-//    }
-//}
-//root(INFO, ["STDOUT"])
+package za.co.entelect.bootcamp.twoface.squareeyes.web.logback
 
 import ch.qos.logback.classic.encoder.PatternLayoutEncoder
 import ch.qos.logback.core.ConsoleAppender
+import ch.qos.logback.core.FileAppender
+
 import static ch.qos.logback.classic.Level.DEBUG
 
 appender("STDOUT", ConsoleAppender) {
     encoder(PatternLayoutEncoder) {
-        //pattern = "%d{HH:mm:ss.SSS} [%thread] %-5level %logger{36} - %msg%n"
         pattern = "%d{HH:mm:ss.SSS} [%thread] %-5level %class{36}.%M %L - %msg%n"
     }
 }
@@ -39,4 +21,3 @@ appender("FILE", FileAppender){
     }
 }
 root(DEBUG, ["STDOUT", "FILE"])
-
