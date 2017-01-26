a24631a2099e5b1d106b9fe1c9c1d4ee1911d5a8
Sean Vienings <sean.vienings@entelect.co.za>
2017-01-26 08:42:28 +0200

    Checkout and cart linking creating

--- web/public/global.js
+++ web/public/global.js
@@ -151,25 +151,3 @@ function hideThis(){
 		document.getElementById('content').className = ""
 	}
 }
-
-function populateCheckoutCart(shoppingCart){
-    var shoppingItems = document.getElementById("shoppingCart")
-
-    if(localStorage.getItem("shoppingCart") != null){
-        shoppingCart = JSON.parse(localStorage.getItem("shoppingCart"))
-        console.log(shoppingCart);
-        var string = "<table class='table table-condensed col-xs-12'><thead><tr><th>Title</th><th>Condition</th><th>Price</th></tr></thead><tbody>"
-
-        for(var x = 0; x < shoppingCart.length; x++){
-
-            string += "<tr><td>" + shoppingCart[x].Title + "</td><td>" + "Very Fine" + "</td><td>R" + 255 + "</td></tr>"
-        }
-        string += "</tbody></table><button type='button' class='btn btn-success' onclick='checkout()' id='checkout' style='float:right'>" +
-            "<span class='glyphicon glyphicon-ok' aria-hidden='true'></span> Checkout" +
-            "</button>"
-        shoppingItems.innerHTML = string
-    }
-    else{
-        shoppingItems.innerHTML = "<h4>no items in shopping cart</h4>"
-    }
-}
