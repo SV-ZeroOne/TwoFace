08c370ae354d2137b9898ab683e6282a6fe81849
Sean Vienings <sean.vienings@entelect.co.za>
2017-01-26 08:15:37 +0200

    Formatting of checkout page

--- web/public/Checkout.js
+++ web/public/Checkout.js
@@ -1,12 +1,14 @@
 //document.getElementById('remove-me').className = "hide"
 var page = 1;
 var itemsOnPage = 8;
+var shoppingCart
 
-if(location.search.includes('page=')){
-	page = location.search.split('page=')[1]
+if(location.search.includes('cart-display')){
+	page = location.search('cart-display');
 }
 
-
+populateShoppingCart(shoppingCart)
+//populateCheckoutCart(shoppingCart)
 populateCatalogue(page, itemsOnPage)
 
 populatePaging(page);
