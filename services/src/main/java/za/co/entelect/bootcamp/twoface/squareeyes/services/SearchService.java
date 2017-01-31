c6bea6b718447402b2f00d407b976974fc8dee77
Sean Vienings <sean.vienings@entelect.co.za>
2017-01-31 17:02:49 +0200

    removed unused imports

--- services/src/main/java/za/co/entelect/bootcamp/twoface/squareeyes/services/SearchService.java
+++ services/src/main/java/za/co/entelect/bootcamp/twoface/squareeyes/services/SearchService.java
@@ -1,11 +1,5 @@
 package za.co.entelect.bootcamp.twoface.squareeyes.services;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import za.co.entelect.bootcamp.twoface.squareeyes.domain.issue.Issue;
-import za.co.entelect.bootcamp.twoface.squareeyes.persistence.relational.issues.IssuesRepository;
-import za.co.entelect.bootcamp.twoface.squareeyes.persistence.relational.issues.IssuesRepositoryIMP;
-
 /**
  * Created by sean.vienings on 2017/01/31.
  *
