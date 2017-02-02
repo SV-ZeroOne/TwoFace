8c3477bda5625aced7ce35058c5f8c01acb2072a
Sean Vienings <sean.vienings@entelect.co.za>
2017-02-02 13:24:51 +0200

    Checkout page and homepage carosel

--- web/src/main/java/za/co/entelect/bootcamp/twoface/squareeyes/web/controller/CheckoutController.java
+++ web/src/main/java/za/co/entelect/bootcamp/twoface/squareeyes/web/controller/CheckoutController.java
@@ -1,7 +1,22 @@
 package za.co.entelect.bootcamp.twoface.squareeyes.web.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 /**
  * Created by sean.vienings on 2017/01/31.
+ *
  */
+@Controller
 public class CheckoutController {
+
+    @RequestMapping(value = "/checkout", method = RequestMethod.GET)
+    public String PopulateCheckout(@RequestParam(value = "search", required = false, defaultValue = "") String search,
+                           @RequestParam(value = "page",  required = false, defaultValue = "1") int page,
+                           ModelMap modelMap){
+        return "checkout";
+    }
 }
